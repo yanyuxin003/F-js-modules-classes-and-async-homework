e0affae769074ae7c5ce939f532942094f7a2efd
yanyuxin <617702281@qq.com>
2020-11-15 12:13:53 +0800

    finish practice 2 and 3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,17 @@
-export const getPoetry
+const url = "https://v1.jinrishici.com/all.json";
+export const getPoetry = () => {
+  const result = fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((res) => {
+      return [res.origin, res.author, res.content];
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return result;
+};
